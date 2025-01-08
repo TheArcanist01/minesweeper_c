b7f0462c57deac15344e83d433aaf4e470c46a4a
TheArcanist <99442890+TheArcanist01@users.noreply.github.com>
2025-01-08 09:57:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include "minelib.h"
 
-int StopFlag = 0;
-
 int main () {
 
     SetConsoleOutputCP(CP_UTF8);
 
-    board_t *Board = initialize_game(15, 15, 10, 15);
+    board_t *Board = initialize_game(6, 6, 10, 15);
 
-    reveal(&Board->Cells[18]);
+    make_move(Board, 0, 0);
+    make_move(Board, 0, 1);
     
+    flag_cell(&Board->Cells[0]);
+
     print_board(Board);
 
     free_board (Board);
